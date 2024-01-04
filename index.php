e957b7fd96529d7bc6ed2dc3bc9c3a163b44a1f3
Cox <ky0145vk@bemidjistate.edu>
2024-01-04 17:14:08 -0600

    added index php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$name='Justin';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <H1>Hello <?php echo $name ?></H1>
+</body>
+</html>
