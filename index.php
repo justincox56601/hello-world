6f321cb4248833a21301c8c4b4e024c5f1030e9b
Cox <ky0145vk@bemidjistate.edu>
2024-01-09 19:00:02 -0600

    added boilerplate

--- index.php
+++ index.php
@@ -1,16 +1,11 @@
-<?php 
-
-$name='Justin';
-?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Hello World</title>
 </head>
 <body>
-    <H1>Hello <?php echo $name ?></H1>
+    <h1>Hello Justin</h1>
 </body>
 </html>
